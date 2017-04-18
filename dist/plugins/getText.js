1f6600a73c542a335b6db232240aaf16c18603de
Nik Coughlin <nrkn.com@gmail.com>
2017-04-18 15:56:16 +1200

    Forgot to build :/
    This is why I should use Andy's deploy tools

--- dist/plugins/getText.js
+++ dist/plugins/getText.js
@@ -2,22 +2,18 @@
 
 var is = require('@mojule/is');
 
-var text = function text(node) {
-  if (is.array(node)) return node.map(text).join('');
-
-  if (node.isText()) return node.nodeValue();
-
-  var children = node.getChildren();
-
-  if (children.length > 0) return text(children);
-
-  return '';
-};
-
 var getText = function getText(node) {
   return {
     getText: function getText() {
-      return text(node);
+      if (node.isText()) return node.nodeValue();
+
+      var children = node.getChildren();
+
+      if (children.length > 0) return children.reduce(function (text, child) {
+        return text + child.getText();
+      }, '');
+
+      return '';
     }
   };
 };

2109f915627b157c1254de04ac42b7e1356077b5
Nik Coughlin <nrkn.com@gmail.com>
2017-07-14 14:14:21 +1200

    .matches available on any node but always false for non-tags

--- src/plugins/api/element/methods/matches.js
+++ src/plugins/api/element/methods/matches.js
@@ -1,15 +1,14 @@
 'use strict'
 
 const CSSselect = require( 'css-select' )
+const is = require( '@mojule/is' )
 const adapter = require( '../../../../css-select-adapter' )
 
 const options = { adapter }
 
 const matches = ({ api, state, core }) => {
-  if( !api.isElementNode() ) return
-
   api.matches = selector => {
-    if( api.isDocumentFragmentNode() ) return false
+    if( !is.string( api.tagName ) ) return false
 
     const { getApi } = core
     const current = getApi( state )
